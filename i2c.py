756a545958f909e1574af953def077ea4118f49e
forrestgtran <forrestgtran@gmail.com>
2017-04-30 16:14:20 -0400

    i2c python

---
+++ i2c.py
@@ -0,0 +1,24 @@
+import time
+import smbus
+
+
+bus = smbus.SMBus(1)
+
+address = 0x04
+
+def writeNumber(value):
+
+	bus.write_byte(address, ord(value))
+
+	return -1
+
+
+while True:
+	var = input("Enter a character: ")
+	
+	if not var:
+		continue
+	
+	writeNumber(var)
+	print("Sent: ", var)
+	time.sleep(1)
