3dc16c3c9fe9202dcf6f73d509c5d0c65366d34b
sotphium <giorgio@brown.edu>
2017-05-01 10:29:08 -0400

    Update raspi_sender_expanded.py

--- raspi_sender_expanded.py
+++ raspi_sender_expanded.py
@@ -1,3 +1,6 @@
+
+#This code lets you choose which Arduino to send i2c serial commands to.
+
 import time
 import smbus
 
@@ -7,13 +10,7 @@ address = 0
 address1 = 0x04
 address2 = 0x06
 
-def writeNumber(value):
-
-	for character in str(value): # convert into a string and iterate over it
-        bus.write_byte(address, ord(character)) # send each char's ASCII encoding
-
-	return -1
-
+# while loop prompts which Arduino to choose (1 or 2)
 while address == 0:
 
     arduino = input("Select Arduino to communicate with: ")
@@ -25,6 +22,14 @@ while address == 0:
     else:
         print("Invalid entry: choose 1 or 2\n")
         continue
+
+
+def writeNumber(value):
+
+	for character in str(value): # convert into a string and iterate over it
+        	bus.write_byte(address, ord(character)) # send each char's ASCII encoding
+
+	return -1
 
 while True:
 	#var = input("Enter a character: ")
