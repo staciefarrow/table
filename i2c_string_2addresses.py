574bc55791a4b595188beb8b8e6bf15026455f53
forrestgtran <forrestgtran@gmail.com>
2017-05-01 19:05:41 -0400

    I2C using 2 arduinos
    Example: 1d50 will send the string d50 to Arduino 1, 2ffaa00 will send the string ffaa00 to Arduino 2

---
+++ i2c_string_2addresses.py
@@ -0,0 +1,40 @@
+#This code lets you choose which Arduino to send i2c serial commands to.
+
+import time
+import smbus
+
+bus = smbus.SMBus(1)
+
+address = 0
+address1 = 0x04
+address2 = 0x06
+
+def writeNumber(value):
+
+        outString = str(value)
+
+        if(outString[0] == '1'):
+                address = address1
+        elif(outString[0] == '2'):
+                address = address2
+        else:
+                print("Error: incorrect Arduino address, choose 1 or 2.")
+        
+	outString = outString[1:]
+        
+	for character in outString: # convert into a string and iterate over it
+        	bus.write_byte(address, ord(character)) # send each char's ASCII encoding
+	return -1
+
+while True:
+
+	try:
+		var = raw_input("Enter a string: ")
+	except ValueError:
+        
+		print("Try again")
+		continue
+
+	writeNumber(var)
+	print("Wrote: "+ var)
+time.sleep(1)
